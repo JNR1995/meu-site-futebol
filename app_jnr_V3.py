73ea76eb2322b2966c847712322b639937e14972
JNR1995 <Junior9547@gmail.com>
2026-05-12 21:33:52 -0300

    Update app_jnr_V3.py

--- app_jnr_V3.py
+++ app_jnr_V3.py
@@ -665,7 +665,7 @@ elif st.session_state.pagina == 'jogos_dia':
                             st.markdown('<div class="container-expectativas">', unsafe_allow_html=True)
 
                             st.markdown(f"""
-                                    <div style='font-size: 13px; color: #787878; margin-bottom: -1px; margin-top: -30px;'><strong>__________________________________________________________________________________________________________________________________________________</strong></div>
+                                    <div style='font-size: 13px; color: #333333; margin-bottom: -1px; margin-top: -30px;'><strong>__________________________________________________________________________________________________________________________________________________</strong></div>
                                 """, unsafe_allow_html=True)
                             
                             col_exp1, col_exp2 = st.columns(2)
@@ -677,7 +677,7 @@ elif st.session_state.pagina == 'jogos_dia':
                                 """, unsafe_allow_html=True)
         
                                 st.markdown(f"""
-                                    <div style='font-size: 13px; color: #bbb; margin-top: -1px; margin-bottom: -6px;'><strong>Expc. Gols Jogo (Média)</strong></div>
+                                    <div style='font-size: 13px; color: #bbb; margin-top: -1px; margin-bottom: -5px;'><strong>Expc. Gols Jogo (Média)</strong></div>
                                     <div style='font-size: 18px; font-weight: bold; margin-top: 0;'>⚽ {exp_gols_g:.2f} &nbsp; | &nbsp; 🏠-✈️ {exp_gols_m:.2f}</div>
                                 """, unsafe_allow_html=True)
                                     
@@ -699,7 +699,7 @@ elif st.session_state.pagina == 'jogos_dia':
                                 """, unsafe_allow_html=True)
         
                                 st.markdown(f"""
-                                    <div style='font-size: 13px; color: #bbb; margin-top: -1px; margin-bottom: -6px;'><strong>Expc. Gols HT (MD)</strong></div>
+                                    <div style='font-size: 13px; color: #bbb; margin-top: -1px; margin-bottom: -5px;'><strong>Expc. Gols HT (MD)</strong></div>
                                     <div style='font-size: 18px; font-weight: bold; margin-top: 0;'>⚽ {exp_gols_ht_g:.2f} &nbsp; | &nbsp; 🏠-✈️ {exp_gols_ht_m:.2f}</div>
                                 """, unsafe_allow_html=True) 
                                     
