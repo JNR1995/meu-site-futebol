f0e3566e21be3c3671c0d97dc31bc0faeebb8dfd
JNR1995 <Junior9547@gmail.com>
2026-05-12 21:15:35 -0300

    Update app_jnr_V3.py

--- app_jnr_V3.py
+++ app_jnr_V3.py
@@ -673,7 +673,7 @@ elif st.session_state.pagina == 'jogos_dia':
                             with col_exp1:
                                 st.markdown(f"""
                                     <div style='font-size: 13px; color: #bbb; margin-bottom: 1px; margin-top: -15px;'><strong>Prob. Over 2.5 Jogo %</strong></div>
-                                    <div style='font-size: 18px; font-weight: bold; margin-top: -5;'>🔎 {exp_over25_g:.1f}% &nbsp; | &nbsp; 🏠-✈️ {exp_over25_m:.1f}%</div>
+                                    <div style='font-size: 18px; font-weight: bold; margin-top: -10;'>🔎 {exp_over25_g:.1f}% &nbsp; | &nbsp; 🏠-✈️ {exp_over25_m:.1f}%</div>
                                 """, unsafe_allow_html=True)
         
                                 st.markdown(f"""
